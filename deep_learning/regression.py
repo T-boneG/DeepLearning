e9318da01e2211f58fba3dce5179355ca6d735f0
Tyler Ganter <tylergan29@gmail.com>
2018-10-04 22:08:16 +0300

    logistic regression and cat example complete

--- deep_learning/regression.py
+++ deep_learning/regression.py
@@ -4,191 +4,191 @@ Logistic Regression and Linear Regression
 
 from __future__ import division
 import numpy as np
-from activation_functions import *
+from activation_functions import sigmoid_af
 
-#TODO make format comparable to scikit learn (all features)
-#TODO make it object oriented
 #TODO split to linear and logistic
 
-def initialize(dim):
+def logistic_forward_propagate(X, w, b):
     """
-    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
-
-    Argument:
-    dim -- size of the w vector we want (or number of parameters in this case)
-
-    Returns:
-    w -- initialized vector of shape (dim, 1)
-    b -- initialized scalar (corresponds to the bias)
+    :param X: vector of shape (dim, number of samples)
+    :param w: vector of shape (dim, 1)
+    :param b: scalar
+    :return: activation function output
     """
-
-    w = np.zeros((dim, 1))
-    b = 0
-
-    assert (w.shape == (dim, 1))
+    assert (w.shape == (X.shape[0], 1))
     assert (isinstance(b, float) or isinstance(b, int))
 
-    return w, b
+    Z = np.dot(w.T, X) + b
+    A = sigmoid_af(Z)
 
-def propagate(w, b, X, Y):
+    return A
+
+def logistic_backward_propagate(X, Y, A):
     """
-    Implement the cost function and its gradient for the propagation explained above
-
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of size (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
-
-    Return:
-    cost -- negative log-likelihood cost for logistic regression
-    dw -- gradient of the loss with respect to w, thus same shape as w
-    db -- gradient of the loss with respect to b, thus same shape as b
+    :param X: vector of shape (dim, number of samples)
+    :param Y: vector of shape (1, number of samples)
+    :param A: vector of shape (1, number of samples)
+    :return: gradients for w and b
     """
+    assert Y.shape == (1, X.shape[1])
+    assert A.shape == Y.shape
 
     m = X.shape[1]
 
-    # FORWARD PROPAGATION (FROM X TO COST)
-    A = sigmoid_af(np.dot(w.T, X) + b)  # compute activation
-    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))  # compute cost
-
-    # BACKWARD PROPAGATION (TO FIND GRAD)
     dw = 1 / m * np.dot(X, (A - Y).T)
     db = 1 / m * np.sum(A - Y)
 
-    assert (dw.shape == w.shape)
-    assert (db.dtype == float)
-    cost = np.squeeze(cost)
-    assert (cost.shape == ())
+    return dw, db
 
-    grads = {"dw": dw,
-             "db": db}
-
-    return grads, cost
-
-def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+def logistic_compute_cost(Y, A):
     """
-    This function optimizes w and b by running a gradient descent algorithm
-
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of shape (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
-    num_iterations -- number of iterations of the optimization loop
-    learning_rate -- learning rate of the gradient descent update rule
-    print_cost -- True to print the loss every 100 steps
-
-    Returns:
-    params -- dictionary containing the weights w and bias b
-    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    :param Y: vector of shape (1, number of samples)
+    :param A: vector of shape (1, number of samples)
+    :return: cost J(w, b)
     """
+    assert Y.shape[0] == 1
+    assert A.shape == Y.shape
 
-    costs = []
+    m = Y.shape[1]
 
-    for i in range(num_iterations):
+    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
 
-        # Cost and gradient calculation
-        grads, cost = propagate(w, b, X, Y)
+    return cost
 
-        # Retrieve derivatives from grads
-        dw = grads["dw"]
-        db = grads["db"]
+class LogisticRegression(object):
+    ###################
+    # Public Functions
+    ###################
 
-        # update rule
-        w = w - learning_rate * dw
-        b = b - learning_rate * db
+    def __init__(self):
+        self.dim = 1
+        self._initialize_params()
 
-        # Record the costs
-        if i % 100 == 0:
+    def get_params(self):
+        return self.params.copy()
+
+    def fit(self, X, Y, num_iterations, learning_rate):
+        """
+        This function optimizes w and b by running a gradient descent algorithm
+
+        Arguments:
+        X -- data of shape (dim, number of examples)
+        Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+        num_iterations -- number of iterations of the optimization loop
+        learning_rate -- learning rate of the gradient descent update rule
+
+        Returns:
+        costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+        """
+        self._update_dim(X.shape[0])
+
+        costs = []
+
+        for i in range(num_iterations):
+            # Cost and gradient calculation
+            grads, cost = self._propagate(X, Y)
+
+            # update rule
+            self.params['w'] = self.params['w'] - learning_rate * grads['dw']
+            self.params['b'] = self.params['b'] - learning_rate * grads['db']
+
             costs.append(cost)
 
-        # Print the cost every 100 training iterations
-        if print_cost and i % 1000 == 0:
-            print ("Cost after iteration %5i: %f" % (i, cost))
+        return costs
 
-    params = {"w": w,
-              "b": b}
+    def predict(self, X):
+        '''
+        Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
 
-    grads = {"dw": dw,
-             "db": db}
+        Arguments:
+        X -- data of size (dim, number of examples)
 
-    return params, grads, costs
+        Returns:
+        Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+        A -- a numpy array (vector) containing probabilities corresponding to predictions
+        '''
+        assert X.shape[0] == self.dim, 'invalid input vector dimension: %d. Expected: %d' % (X.shape[0], self.dim)
 
-def predict(w, b, X):
-    '''
-    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+        A = logistic_forward_propagate(X, self.params['w'], self.params['b'])
 
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of size (num_px * num_px * 3, number of examples)
-
-    Returns:
-    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
-    '''
-
-    m = X.shape[1]
-    Y_prediction = np.zeros((1, m))
-    w = w.reshape(X.shape[0], 1)
-
-    # Compute vector "A" predicting the probabilities of a cat being present in the picture
-    A = sigmoid_af(np.dot(w.T, X) + b)
-
-    for i in range(A.shape[1]):
-        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        # Convert probabilities (A) to actual predictions
         Y_prediction = (A > 0.5).astype(int)
 
-    assert (Y_prediction.shape == (1, m))
+        #TODO delete this after testing
+        assert (Y_prediction.shape == (1, X.shape[1]))
 
-    return Y_prediction
+        return Y_prediction, A
 
-def model(X_train, Y_train, X_test, Y_test, num_iterations=2000,
-          learning_rate=0.5, print_cost=False):
-    """
-    Builds the logistic regression model by calling the function you've implemented previously
+    def score(self, X, Y):
+        """
+        prediction percent correct
+        :param X:
+        :param Y:
+        :return:
+        """
+        assert Y.shape == (1, X.shape[1]), 'invalid input dimensions'
 
-    Arguments:
-    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
-    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
-    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
-    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
-    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
-    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
-    print_cost -- Set to true to print the cost every 100 iterations
+        Y_prediction, _ = self.predict(X)
 
-    Returns:
-    d -- dictionary containing information about the model.
-    """
+        correct = np.sum(Y_prediction == Y)
+        return correct / Y.shape[1]
 
-    # initialize parameters with zeros
-    w, b = initialize(X_train.shape[0])
+    ###################
+    # Private Functions
+    ###################
 
-    # Gradient descent
-    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations,
-                                        learning_rate, print_cost)
+    def _initialize_params(self):
+        """
+        This method creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
 
-    # Retrieve parameters w and b from dictionary "parameters"
-    w = parameters["w"]
-    b = parameters["b"]
+        Argument:
+        dim -- size of the w vector we want (or number of parameters in this case)
 
-    # Predict test/train set examples
-    Y_prediction_test = predict(w, b, X_test)
-    Y_prediction_train = predict(w, b, X_train)
+        sets dictionary:
+        params
+            w -- initialized vector of shape (dim, 1)
+            b -- initialized scalar (corresponds to the bias)
+        """
 
-    # Print train/test Errors
-    print("train accuracy: {} %".format(
-        100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-    print("test accuracy: {} %".format(
-        100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+        w = np.zeros((self.dim, 1))
+        b = 0
 
-    d = {"costs": costs,
-         "Y_prediction_test": Y_prediction_test,
-         "Y_prediction_train": Y_prediction_train,
-         "w": w,
-         "b": b,
-         "learning_rate": learning_rate,
-         "num_iterations": num_iterations}
+        assert (w.shape == (self.dim, 1))
+        assert (isinstance(b, float) or isinstance(b, int))
 
-    return d
+        self.params = {
+            'w': w,
+            'b': b
+        }
+
+    def _update_dim(self, new_dim):
+        if self.dim != new_dim:
+            self.dim = new_dim
+            self._initialize_params()
+
+    def _propagate(self, X, Y):
+        """
+        Implement the cost function and its gradient for the propagation explained above
+
+        Arguments:
+        X -- data of size (dim, number of examples)
+        Y -- true "label" vector  of size (1, number of examples)
+
+        Return:
+        cost -- negative log-likelihood cost for logistic regression
+        dw -- gradient of the loss with respect to w, thus same shape as w
+        db -- gradient of the loss with respect to b, thus same shape as b
+        """
+        assert Y.shape == (1, X.shape[1]), 'invalid input dimensions'
+
+        # FORWARD PROPAGATION (FROM X TO COST)
+        A = logistic_forward_propagate(X, self.params['w'], self.params['b'])
+        cost = logistic_compute_cost(Y, A)
+
+        # BACKWARD PROPAGATION (TO FIND GRAD)
+        dw, db = logistic_backward_propagate(X, Y, A)
+
+        grads = {'dw': dw,
+                 'db': db}
+
+        return grads, cost
