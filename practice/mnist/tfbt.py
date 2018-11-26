8d17e5d8873adead640e969636510bbc756f2795
Tyler Ganter <tyler.h.ganter@gmail.com>
2018-11-26 16:45:37 +0100

    removing practice dir

--- practice/mnist/tfbt.py
+++
@@ -1,9 +0,0 @@
-"""
-TensorFlow Boosted Tree
-
-"""
-from __future__ import absolute_import, division, print_function
-
-import tensorflow as tf
-
-model = tf.estimator.BoostedTreesClassifier()
--- practice/mnist/xgboost.py
+++
@@ -1,9 +0,0 @@
-"""
-XGBoost
-
-Gradient Boosted Tree for < 1 million samples
-DNN                   for > 1 million samples
-"""
-from __future__ import absolute_import, division, print_function
-
-import xgboost
